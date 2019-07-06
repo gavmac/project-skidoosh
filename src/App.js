a8f422fcdb59fe1e7b4ef8e93a0b490e95283ce2
gavmac <gavinmacken87@gmail.com>
2019-07-05 22:12:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import logo from './logo.svg';
+import './App.css';
+
+import Hiro from './Hiro'
+
+class App extends Component {
+    render() {
+        return (
+            <Hiro />
+        );
+    }
+}
+
+
+export default App;
