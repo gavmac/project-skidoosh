0bcfc087bbf1f36b9e9a4096e26cb1a038df2b4d
gavmac <gavinmacken87@gmail.com>
2019-07-09 17:30:19 -0400

    css baseline - browser support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
 import Header from './components/Header';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <CssBaseline />
                 <Header />
                 <Routes />
             </div>
