c31352c7c574937fe9f3a8a9b509118685ab23eb
gavmac <gavinmacken87@gmail.com>
2019-07-30 18:44:21 -0400

    some-changes

--- src/server/index.js
+++ src/server/index.js
@@ -34,12 +34,11 @@ app.listen( process.env.PORT || 4000, () =>
 
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
-    app.use(express.static(__dirname));
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/ping', function (req, res) {
-        return res.send('pong');
-    });
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
