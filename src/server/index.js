8d0c3139163fb6f0307c5ca064ffe1a254b1d2b6
gavmac <gavinmacken87@gmail.com>
2019-07-30 14:53:29 -0400

    some-changes

--- src/server/index.js
+++ src/server/index.js
@@ -33,11 +33,8 @@ app.listen( process.env.PORT || 4000, () =>
 );
 
 if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('build'));
 
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
