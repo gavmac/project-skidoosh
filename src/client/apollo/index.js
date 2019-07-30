9ff65dedaa766bf0f2176b032d7ec52f214bb242
gavmac <gavinmacken87@gmail.com>
2019-07-30 15:27:22 -0400

    some-changes

--- src/client/apollo/index.js
+++ src/client/apollo/index.js
@@ -3,12 +3,14 @@ import { createUploadLink } from 'apollo-upload-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const token = localStorage.getItem('token');
-const PORT = process.env.PORT || 4000;
 
+const { NODE_ENV, REACT_APP_GRAPHQL_URI } = process.env;
+const isNotProduction = NODE_ENV !== 'production';
+const uri = isNotProduction ? 'http://localhost:4000/graphql' : REACT_APP_GRAPHQL_URI;
 
 // 2
 const Link = createUploadLink({
-    uri: `http://localhost:${PORT}/graphql`,
+    uri: {uri},
     headers: {
         authorization: token ? `Bearer ${token}` : ''
     }
