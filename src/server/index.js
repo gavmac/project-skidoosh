a19907899fe4723006dcaa054326a230ff1b7993
gavmac <gavinmacken87@gmail.com>
2019-07-30 14:48:57 -0400

    some-changes

--- src/server/index.js
+++ src/server/index.js
@@ -32,6 +32,13 @@ app.listen( process.env.PORT || 4000, () =>
     console.log(`🚀 Server ready at http://localhost:${PORT}`)
 );
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'build/index.html'))
-});
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
