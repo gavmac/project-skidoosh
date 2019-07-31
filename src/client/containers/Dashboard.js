ff1d9f6967d7c11013ad217023ccc773db2274eb
gavmac <gavinmacken87@gmail.com>
2019-07-30 20:22:46 -0400

    some-changes

--- src/client/containers/Dashboard.js
+++ src/client/containers/Dashboard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 
-
-import Dropzone from 'react-dropzone'
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
+import client from "../apollo";
 
 const uploadFileMutation = gql `
     mutation($file: Upload!) {
@@ -12,25 +13,58 @@ const uploadFileMutation = gql `
       }
     }
 `;
+//
+// const dashBoard = () => {
+//     return (
+//         <Mutation mutation={uploadFileMutation}>
+//             {mutate => (
+//                 <Dropzone onDrop={([file]) => mutate({variables: { file }})}>
+//                     {({getRootProps, getInputProps}) => (
+//                         <section>
+//                             <div {...getRootProps()}>
+//                                 <input {...getInputProps()} />
+//                                 <p>Drop it in drop it in, drop it in drop it in.... oh oh oooooh</p>
+//                             </div>
+//                         </section>
+//                     )}
+//
+//                 </Dropzone>
+//             )}
+//         </Mutation>
+//     )
+// };
+
+
 
 const dashBoard = () => {
-    return (
-        <Mutation mutation={uploadFileMutation}>
-            {mutate => (
-                <Dropzone onDrop={([file]) => mutate({variables: { file}})}>
-                    {({getRootProps, getInputProps}) => (
-                        <section>
-                            <div {...getRootProps()}>
-                                <input {...getInputProps()} />
-                                <p>Drop it in drop it in, drop it in drop it in.... oh oh oooooh</p>
-                            </div>
-                        </section>
-                    )}
 
-                </Dropzone>
-            )}
-        </Mutation>
-    )
-};
+        const handleChangeStatus = ({ meta, file }, status) => {
+            console.log(status, meta, file)
+        }
+
+        const handleSubmit = async (files) => {
+            console.log('yo')
+            console.log(files[0].file);
+            try {
+                await client.mutate({
+                    variables: files[0],
+                    mutation: uploadFileMutation,
+                });
+            } catch (errors) {
+                return errors.message
+            }
+        }
+
+        return (
+            <Dropzone
+                onChangeStatus={handleChangeStatus}
+                onSubmit={handleSubmit}
+                maxFiles={1}
+                inputContent="Drop 1 File"
+            />
+        )
+    };
+
+
 
 export default dashBoard;
